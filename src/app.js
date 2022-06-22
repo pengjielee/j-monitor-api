90b36ce3ffc774d6116e6f90239d090fb1c4df17
pengjielee <386276251@qq.com>
2022-06-22 11:30:42 +0800

    feat: 连接mongodb

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Log = require('./db/log');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/test', async (req, res) => {
+	const logs = await Log.find();
+	console.log(logs);
+  res.send(logs);
 })
 
 app.listen(port, () => {
---
+++ src/db/log.js
@@ -0,0 +1,9 @@
+const mongoose = require('./mongoose');
+
+const logSchema = new mongoose.Schema({
+  name: String
+});
+
+const logModel = mongoose.model('Log', logSchema);
+
+module.exports = logModel;
