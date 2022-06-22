f1eeab9511fa524b353b6d673f0bebaacb3e7ae7
pengjielee <386276251@qq.com>
2022-06-22 13:29:29 +0800

    feat: 安装cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,50 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 const Log = require("./db/log");
 
-app.get("/", (req, res) => {
+// app.use(cors({
+// 	origin: ['https://www.baidu.com']
+// }))
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/test", async (req, res) => {
+app.get("/test1", async (req, res) => {
   const logs = await Log.find();
   console.log(logs);
   res.send(logs);
+});
+
+app.get("/test2", (req, res) => {
+  const names = [
+    {
+      id: "1",
+      item: "Bacon",
+    },
+    {
+      id: "2",
+      item: "Eggs",
+    },
+    {
+      id: "3",
+      item: "Milk",
+    },
+    {
+      id: "4",
+      item: "Butter",
+    },
+  ];
+  res.send(names);
 });
 
 app.listen(port, () => {
